07637568f0bd3377db32ba34c503bfa79004997f
DeniskaStar <Popcorn2008@mail.ru>
2021-12-22 21:25:24 +0700

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,20 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    public static Connection getConnect() {
+        final String URL = "jdbc:mysql://localhost:3306/mydbtest?serverTimezone=Europe/Moscow&useSSL=false";
+        final String USERNAME = "root";
+        final String PASSWORD = "root";
+
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
